8912a9898e4d5e34643ee6eae43ee2ce1acc3866
VanJason <30817438+VanJason@users.noreply.github.com>
2017-08-10 15:52:55 +0800

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -11,12 +11,11 @@ from programme_for_gzcw import *
 
 class MainWindow(QMainWindow, Ui_MainWindow,Programme_guoyi,Programme_gdcw,Programme_gzcw):
 
-    _signal_web=QtCore.pyqtSignal(str)
-    _signal_type=QtCore.pyqtSignal(str)
-    _signal_buyer=QtCore.pyqtSignal(str)
-    _signal_title=QtCore.pyqtSignal(str)
-    _signal_filename=QtCore.pyqtSignal(str)
-    _signal6=QtCore.pyqtSignal()
+
+    # _signal6=QtCore.pyqtSignal()
+
+    listrecord = [None,None,None,None,None]
+    infolist = [None,None,None,None,None,None,None,None]
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -48,15 +47,7 @@ class MainWindow(QMainWindow, Ui_MainWindow,Programme_guoyi,Programme_gdcw,Progr
         self.P_showtime.toggled['bool'].connect(self.getshowtime)
         self.P_buyer.toggled['bool'].connect(self.getbuyer)
 
-        self._signal_web.connect(self.sin_web)
-        self._signal_type.connect(self.sin_type)
-        self._signal_buyer.connect(self.sin_buyer)
-        self._signal_title.connect(self.sin_title)
-        self._signal_filename.connect(self.sin_filename)
-        self._signal6.connect(self.sin6)
-
-        
-
+        # self._signal6.connect(self.sin6)
 
     """
     单项选择——招标网站选择方法
@@ -65,23 +56,19 @@ class MainWindow(QMainWindow, Ui_MainWindow,Programme_guoyi,Programme_gdcw,Progr
     """
     def guoyiWeb(self,state):
         if state is True:
-            self._signal_web.emit("guoyi")
+            self.listrecord[0] = "guoyi"
         else:
             None
     def gdcw(self,state):
         if state is True:
-            self._signal_web.emit("gdcw")
+            self.listrecord[0] = "gdcw"
         else:
             None
     def gzcw(self,state):
         if state is True:
-            self._signal_web.emit("gzcw")
+            self.listrecord[0] = "gzcw"
         else:
             None
-
-    def sin_web(self,string):
-        print(string)
-
 
 
     """
@@ -91,79 +78,104 @@ class MainWindow(QMainWindow, Ui_MainWindow,Programme_guoyi,Programme_gdcw,Progr
     """
     def zhaobiao(self,state):
         if state is True:
-            self._signal_type.emit("招标公告")
+            self.listrecord[1] = "招标公告"
         else:
             None    
     def zhongbiao(self,state):
         if state is True:
-            self._signal_type.emit("中标公告")
+            self.listrecord[1] = "中标公告"
         else:
             None
-
-    def sin_type(self,string):
-        print(string)
 
     """
     填写生成文件
     定义信号
     """
     def filename(self,strtext):
-        self._signal_filename.emit(strtext)
-
-    def sin_filename(self,string):
-        print(string)
+        self.strtext = strtext
+        self.listrecord[2] = self.strtext
 
     """
     填写检索关键词
     定义信号
     """    
     def caigouren(self,strtext):
-        self._signal_buyer.emit(strtext)
+        self.strtext = strtext
+        self.listrecord[3] = self.strtext
     def biaoti(self,strtext):
-        self._signal_title.emit(strtext)
-
-    def sin_buyer(self,string):
-        print(string)
-    def sin_title(self,string):
-        print(string)
+        self.strtext = strtext
+        self.listrecord[4] = self.strtext
 
     """多选——选择要获取的信息"""
     def gettitle(self,state):
-        pass      
+        self.state = state
+        if state is True:
+            self.infolist[0] = self.state 
+        else:
+            self.infolist[0] = None     
     def getaccount(self,state):
         self.state = state
-        return self.state         
+        if state is True:
+            self.infolist[1] = self.state 
+        else:
+            self.infolist[1] = None          
     def getbeginningtime(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[2] = self.state 
+        else:
+            self.infolist[2] = None 
     def getagentcompany(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[3] = self.state 
+        else:
+            self.infolist[3] = None 
     def getlink(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[4] = self.state 
+        else:
+            self.infolist[4] = None 
     def getsellaccount(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[5] = self.state 
+        else:
+            self.infolist[5] = None  
     def getshowtime(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[6] = self.state 
+        else:
+            self.infolist[6] = None 
     def getbuyer(self,state):
         self.state = state
-        return self.state 
+        if state is True:
+            self.infolist[7] = self.state 
+        else:
+            self.infolist[7] = None 
 
     def setCreatOracl(self):
         pass
     	# QtWidgets.QMessageBox.information(self.creatExcel,"标题","生成Excel成功")
     def setCreatExcel(self):
-        self._signal6.emit()
+        if self.listrecord[0] =="guoyi":
+            url = 'http://www.gmgit.com/'
+            info = Programme_guoyi.get_Web_guoyi(self,url,self.listrecord[1],self.listrecord[4])
+            WBall = Programme_guoyi.get_programme_guoyi(self,info)
+            Programme_guoyi.get_detail_guoyi(self,WBall,self.listrecord[2],self.infolist[0],self.infolist[1],self.infolist[2],self.infolist[3],self.infolist[4],self.infolist[5],self.infolist[6],self.infolist[7])
+            QtWidgets.QMessageBox.information(self.creatExcel,"标题","生成国义招标网爬虫成功")
+        elif self.listrecord[0] =="gdcw":
+            None
+        elif self.listrecord[0] == "gzcw":
+            None
+        else:
+            QtWidgets.QMessageBox.information(self.creatExcel,"标题","请选择要爬虫的网站")
 
         #     QtWidgets.QMessageBox.information(self.creatExcel,"标题","正在生成Excel")
         # except:
         #     QtWidgets.QMessageBox.information(self.creatExcel,"标题","信息填写错误")
-
-    def sin6(self):
-        print("test")
 
 
 
